d2eb28cd15d8b4e133795d56eb242757c8e517d3
Kim Sehyun <kkshyun56@gmail.com>
2024-05-21 09:23:19 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [name, setName] = useState(0);
+  const [title, setTitle] = useState(0);
+  const [data, setData] = useState();
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/v1/boards");
+      setData(response.data);
+      console.log(response);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const postData = async () => {
+    try {
+      const response = await axios.post("http://localhost:8080/api/v1/boards", {
+        userName: name,
+        title: title,
+      });
+      console.log(response);
+      getData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <input type="name" onChange={(e) => setName(e.target.value)}></input>
+      <input type="title" onChange={(e) => setTitle(e.target.value)}></input>
+      <button onClick={postData}>submit</button>
+      {data?.map((element) => (
+        <div>
+          name:{element.userName} title:{element.boardTitle}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
